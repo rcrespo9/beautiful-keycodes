9b78e01115920e4b84a5d9fddc3fb6a633699bd9
rudycrespo <rudy.crespo20@gmail.com>
2016-12-30 16:10:32 -0500

    created canvas shapes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,3 +28,38 @@ window.addEventListener('keydown', function(e) {
 	number.innerHTML = keyCode;
 	text.innerHTML = keyFormatter(keyCode); 
 });
+
+function drawShapes() {
+  const canvas = document.getElementById('js-particles');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+    const strokeWidth = 3;
+    const darkBlue = '#16243c';
+    let triangleXPos = 70;
+    let triangleYPos = 20;
+
+    // circle
+    ctx.beginPath();
+    ctx.arc(75, 75, 6, 0, Math.PI * 2, false);
+    ctx.lineWidth = strokeWidth;
+    ctx.strokeStyle = darkBlue;
+    ctx.stroke();
+
+    // square
+    ctx.beginPath();
+    ctx.rect(30, 30, 12, 12);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = darkBlue;
+    ctx.stroke();
+
+    // triangle
+    ctx.beginPath();
+    ctx.fillStyle = darkBlue;
+    ctx.moveTo(triangleXPos, triangleYPos);
+    ctx.lineTo(triangleXPos - 10, triangleYPos + 12); 
+    ctx.lineTo(triangleXPos + 10, triangleYPos + 12);
+    ctx.fill();
+  }
+}
+
+drawShapes();
