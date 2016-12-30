5d8955a481ffce3ff318bba570f8cfb9d721f260
rudycrespo <rudy.crespo20@gmail.com>
2016-12-29 21:58:06 -0500

    key formatter function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,9 +7,24 @@ window.addEventListener('keydown', function(e) {
 	let keyCode = e.keyCode;
 	let key = e.key;
 
-	console.log(e);
+	function keyFormatter(keycode) {
+		var specialKeys = {
+			20: 'caps lock',
+			32: 'spacebar',
+			37: 'arrow left',
+			38: 'arrow up',
+			39: 'arrow right',
+			40: 'arrow down'
+		}
+
+		if(specialKeys[keycode]) {
+			return specialKeys[keycode]; 
+		} else {
+			return key;
+		}
+	}
+
 
 	number.innerHTML = keyCode;
-
-	text.innerHTML = key; 
+	text.innerHTML = keyFormatter(keyCode); 
 });
