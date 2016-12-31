f994444369c775ae849c1d52bd72a33ebc960bab
rudycrespo <rudy.crespo20@gmail.com>
2016-12-31 01:25:37 -0500

    almost animated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,61 +33,104 @@ function getRandomNum(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-function drawShapes(keycode = 36) {
-  const canvas = document.getElementById('js-particles');
+const canvas = document.getElementById('js-particles');
+const ctx = canvas.getContext('2d');
+const globalLineWidth = 3;
+const darkBlue = '#16243c';
+let circles = [];
+
+function Circle(radius, width, xPos, yPos) {
+	this.radius = radius;
+	this,width = width;
+	this.xPos = xPos;
+	this.yPos = yPos;	
+	this.counter = 0;
+}
+
+Circle.prototype.update = function() {
+	this.counter++;
+	
+    ctx.beginPath();
+   	ctx.arc(this.xPos + this.counter, this.yPos + this.counter, this.radius, 0, Math.PI * 2, false);
+    ctx.lineWidth = globalLineWidth;
+    ctx.strokeStyle = darkBlue;
+    ctx.stroke();
+    ctx.closePath();	
+}
+
+function Square(width, height, xPos, yPos) {
+	this.radius = radius;
+	this,width = width;
+	this.xPos = xPos;
+	this.yPos = yPos;	
+}
+
+function Triangle(firstLine, secondLine, xPos, yPos) {
+	this.firstLine = firstLine;
+	this,secondLine = secondLine;
+	this.xPos = xPos;
+	this.yPos = yPos;	
+}
+
+var drawShapes = function(keycode = 10) {
   let numShapes = Math.ceil((keycode / 3) * 1) / 1;
 
   if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const globalLineWidth = 3;
-    const darkBlue = '#16243c';
+    // circle    	
+ 	for(let i = 0; i < numShapes; i++) {
+ 		let randX = getRandomNum(0, canvas.width);
+		let randY = getRandomNum(0, canvas.height);
+ 		let circle = new Circle(6, 0, randX, randY);
 
-    // circle
-    for(let i = 0; i < numShapes; i++) {
-    	let randX = getRandomNum(0, canvas.width);
-	    let randY = getRandomNum(0, canvas.height);
-
-    	ctx.beginPath();
-	   	ctx.arc(randX, randY, 6, 0, Math.PI * 2, false);
-	    ctx.lineWidth = globalLineWidth;
-	    ctx.strokeStyle = darkBlue;
-	    ctx.stroke();
-	    ctx.closePath();
+ 		circles.push(circle);
     }
 
-    // square
-    for(let i = 0; i < numShapes; i++) {
-    	let randX = getRandomNum(0, canvas.width);
-	    let randY = getRandomNum(0, canvas.height);
+    executeFrame();
+ //    // square	    
+	// for(let i = 0; i < numShapes; i++) {
+	// 	const squareRandX = getRandomNum(0, canvas.width);
+	//     const squareRandY = getRandomNum(0, canvas.height);
 
-	    ctx.beginPath();
-	    ctx.rotate(360);
-	    ctx.rect(randX, randY, 12, 12);
-	    ctx.lineWidth = 3;
-	    ctx.strokeStyle = darkBlue;
-	    ctx.stroke();
-	    ctx.closePath();
-	}
+	//     ctx.beginPath();
+	//     ctx.rect(squareRandX, squareRandY, 12, 12);
+	//     ctx.lineWidth = 3;
+	//     ctx.strokeStyle = darkBlue;
+	//     ctx.stroke();
+	//     ctx.closePath();
+	// }
 
-    // triangle
-    for(let i = 0; i < numShapes; i++) {
-	    let randX = getRandomNum(0, canvas.width);
-	    let randY = getRandomNum(0, canvas.height);
+ //    // triangle	    
+	// for(let i = 0; i < numShapes; i++) {
+	// 	const triangleRandX = getRandomNum(0, canvas.width);
+	//     const triangleRandY = getRandomNum(0, canvas.height);
 
-	    ctx.beginPath();
-	    ctx.rotate(360);
-	    ctx.fillStyle = darkBlue;
-	    ctx.moveTo(randX, randY);
-	    ctx.lineTo(randX - 10, randY + 12); 
-	    ctx.lineTo(randX + 10, randY + 12);
-	    ctx.fill();
-	    ctx.closePath();
-	}
+	//     ctx.beginPath();
+	//     ctx.fillStyle = darkBlue;
+	//     ctx.moveTo(triangleRandX, triangleRandY);
+	//     ctx.lineTo(triangleRandX - 10, triangleRandY + 12); 
+	//     ctx.lineTo(triangleRandX + 10, triangleRandY + 12);
+	//     ctx.fill();
+	//     ctx.closePath();
+	// }
   }
-
 }
 
-drawShapes(100);
+console.log(circles);
+
+drawShapes(3);
+
+function executeFrame() {
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	for(let i = 0; i < circles.length; i++) {
+		let circle = circles[i];
+		circle.update();
+	}
+
+    requestAnimationFrame(executeFrame);
+}
+
+// executeFrame();
